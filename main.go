b5142bfcf4f90ffdc52f6c91161be518d98b235d
Oliver Schmid <oliver.schmd@gmail.com>
2012-09-30 16:42:26 -0700

    GPLv3 headers

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
-/**
- * Created with IntelliJ IDEA.
- * User: oschmid
- * Date: 21/09/12
- * Time: 5:22 PM
+/*
+This file is part of Notes.
+
+Notes is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Notes is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 package main
 
