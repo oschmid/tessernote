aef062d893df75b3984d3e9aaf45d48c75c6f539
Oliver Schmid <oliver.schmd@gmail.com>
2012-11-08 23:03:17 -0800

    extract tags, id from editNoteWithTagsURL

--- api/pages.go
+++ api/pages.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"regexp"
+	"strings"
 )
 
 var (
-	hex                     = "[a-fA-F0-9]"
-	tag                     = "[a-zA-Z0-9]" // TODO use twitter regex
-	tags                    = "(" + tag + "+\\,)*" + tag + "+"
-	uniqueId                = hex + "{8}(-" + hex + "{4}){3}-" + hex + "{12}"
-	displayNoteWithAllTags  = regexp.MustCompile("^/" + uniqueId + "$")                   // grivet.ca/<note UUID>
-	editNoteWithAllTags     = regexp.MustCompile("^/" + uniqueId + "/edit$")              // grivet.ca/<note UUID>?edit
-	displayTags             = regexp.MustCompile("^/" + tags + "/$")                      // grivet.ca/<tags>/
-	displayNoteWithSomeTags = regexp.MustCompile("^/" + tags + "/" + uniqueId + "$")      // grivet.ca/<tags>/<note UUID
-	editNoteWithSomeTags    = regexp.MustCompile("^/" + tags + "/" + uniqueId + "/edit$") // grivet.ca/<tags>/<note UUID
+	hexPattern             = "[a-fA-F0-9]"
+	tag                    = "[a-zA-Z0-9]" // TODO use twitter regex
+	tags                   = "((" + tag + "+\\,)*" + tag + "+)"
+	uniqueId               = "(" + hexPattern + "{8}(-" + hexPattern + "{4}){3}-" + hexPattern + "{12})"
+	displayNoteURL         = regexp.MustCompile("^/" + uniqueId + "$")                   // grivet.ca/<note UUID>
+	editNoteURL            = regexp.MustCompile("^/" + uniqueId + "/edit$")              // grivet.ca/<note UUID>/edit
+	displayTagsURL         = regexp.MustCompile("^/" + tags + "/$")                      // grivet.ca/<tags>/
+	displayNoteWithTagsURL = regexp.MustCompile("^/" + tags + "/" + uniqueId + "$")      // grivet.ca/<tags>/<note UUID>
+	editNoteWithTagsURL    = regexp.MustCompile("^/" + tags + "/" + uniqueId + "/edit$") // grivet.ca/<tags>/<note UUID>/edit
 )
 
 func init() {
@@ -23,17 +24,22 @@ func init() {
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	if editNoteWithSomeTags.MatchString(r.URL.Path) {
-		fmt.Fprintf(w, "edit note with some tags")
-	} else if displayNoteWithSomeTags.MatchString(r.URL.Path) {
+	if editNoteWithTagsURL.MatchString(r.URL.Path) {
+		matches := editNoteWithTagsURL.FindStringSubmatch(r.URL.Path)
+		editNoteWithTags(w, r, strings.Split(matches[1], ","), matches[3])
+	} else if displayNoteWithTagsURL.MatchString(r.URL.Path) {
 		fmt.Fprint(w, "display note with some tags")
-	} else if editNoteWithAllTags.MatchString(r.URL.Path) {
+	} else if editNoteURL.MatchString(r.URL.Path) {
 		fmt.Fprint(w, "edit note with all tags")
-	} else if displayNoteWithAllTags.MatchString(r.URL.Path) {
+	} else if displayNoteURL.MatchString(r.URL.Path) {
 		fmt.Fprint(w, "display note with all tags")
-	} else if displayTags.MatchString(r.URL.Path) {
+	} else if displayTagsURL.MatchString(r.URL.Path) {
 		fmt.Fprint(w, "display tags")
 	} else {
 		fmt.Fprint(w, r.URL.Path)
 	}
+}
+
+func editNoteWithTags(w http.ResponseWriter, r *http.Request, tags []string, id string) {
+	fmt.Fprintf(w, "tags:%v\nid:%v", tags, id)
 }
